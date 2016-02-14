fcdff8869903c98d5eb2281d4698113ab88ed212
John Muccigrosso <john@muccigrosso.org>
2016-02-14 14:27:40 -0500

    Add help & missing input file handlers.

--- README.md
+++ README.md
@@ -2,15 +2,27 @@
 
 Just some other scripts that aren't Apple.
 
-- wikitbl.pl: a perl script that converts simple text tables to wikipedia-style tables. Syntax:
+- wikitbl.pl: a perl script that converts simple text tables to wikipedia-style tables.
 
-        wikitbl.pl -c|-t|-s <separator> [-h] <input file> [<output file>]
+        SYNOPSIS
         
-        -c : columns are separated by comma (the default when no option is present)
-        -t : columns are separated by tab
-        -s <separator>: columns are separated by <separator>
-        -h : first row of file is treated as a table header
+        wikitbl.pl [-c|-t|-s <text>] [-h] [-help] <input file> [<output file>]
         
+        DESCRIPTION
+
+        A script to convert simple text tables to simple Wikipedia-style tables. The
+        script assumes that the columns are separated by commas (also can be forced
+        by -c), but will accept tabs (-t) and any other string (-s <text>). If the
+        -h option is used, the script will treat the first line as a header. No
+        effort is made to give all rows an equal number of cells.
+        
+        The following options are available:
+        -c        columns are separated by comma (the default when no option is present)
+        -t        columns are separated by tab
+        -s <text> columns are separated by <text>
+        -h        first row of file is treated as a table header
+        -help     print this information
+                
         If no output file is indicated, the output will go to STDOUT.
         
     Still to do: provide option for one-line-per-cell style.
--- wikitbl.pl
+++ wikitbl.pl
@@ -3,10 +3,37 @@
 my $optionalstring = '';
 
 use Getopt::Long;
-GetOptions("c"=>   \$cflag,
-           "t"=>   \$tflag,
-           "h"=>   \$hflag,
-           "s:s"=> \$sepstring);
+GetOptions("c"=>    \$cflag,
+           "t"=>    \$tflag,
+           "h"=>    \$hflag,
+           "s:s"=>  \$sepstring,
+           "help"=> \$helpflag);
+
+# Handle a request for help
+if ($helpflag) {
+	print "SYNOPSIS\n\n";
+	print "	wikitbl.pl [-c|-t|-s <text>] [-h] [-help] <input file> [<output file>]\n\n";
+	print "DESCRIPTION\n\n";
+	print "	A script to convert simple text tables to simple Wikipedia-style tables. The\n";
+	print "	script assumes that the columns are separated by commas (also can be forced\n";
+	print "	by -c), but will accept tabs (-t) and any other string (-s <text>). If the \n";
+	print "	-h option is used, the script will treat the first line as a header. No\n";
+	print "	effort is made to give all rows an equal number of cells.\n\n";
+	print "	The following options are available:\n";
+	print "		-c        columns are separated by comma (the default when no option is present)\n";
+	print "		-t        columns are separated by tab\n";
+	print "		-s <text> columns are separated by <text>\n";
+	print "		-h        first row of file is treated as a table header\n";
+	print "		-help     print this information\n\n";
+	print "	If no output file is indicated, the output will go to STDOUT.\n";
+	exit;
+}
+
+# When no help request or input file.
+if (!$ARGV[0]) {
+	print chr(7),"\nError: Must include at least an input file. Try 'wikitbl.pl -help' for more information.\n\n";
+	exit;
+}
 
 open(INPUT, $ARGV[0]) or die "Couldn't open file for reading: $!\n";
 
