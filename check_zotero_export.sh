8b6ab541044c0a9dbe5e31ce9424204b25379823
John Muccigrosso <john@muccigrosso.org>
2017-06-11 21:42:46 -0400

    Git commit on change

--- check_zotero_export.sh
+++ check_zotero_export.sh
@@ -3,8 +3,20 @@
 # Send email when Zotero library export file has 0 size
 
 me=$(whoami)
+dest="/Users/$me/Documents/github/local/miscellaneous"
+f="My Library.json"
 
-	if [ ! -s "/Users/$me/Documents/github/local/miscellaneous/My Library.json" ]
-	then
-		echo 'Darn it!' | mail -s 'Zotero export failed' john
+if [ ! -s "$dest/$f" ]
+then
+    echo 'Darn it!' | mail -s 'Zotero export failed' john
+else
+    # Commit it to github when it's changed
+    gitList=$(cd "$dest"; git status -s)
+    gitItem=$(echo $gitList | egrep "M \"$f\"")
+    if [ ${#gitItem} -ne 0 ] 
+        then 
+            echo "$(date +%Y-%m-%d\ %H:%M:%S) \"$f\" pushed to github" 1>&2
+            cd "$dest";git commit -m 'Regular update' "$f"
     fi
+fi
+
