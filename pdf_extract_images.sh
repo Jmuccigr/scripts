71088f575773a095c773f9ecec3e22cd256e992f
John Muccigrosso <john@muccigrosso.org>
2017-06-22 16:06:51 -0400

    Handles non-PDF files; spaces in paths

--- pdf_extract_images.sh
+++ pdf_extract_images.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-# Extract and clean up PDFs of articles, text-only
+# Extract and clean up scans of articles, text-only
 #
-# 1. Extract images from PDF
-# 2. Use unpaper to split the pages and do its cleanup
-# 3. Use Fred's textcleaner?
-# 4. Stop here to manually check the files.
+# 1. Extract images from PDF, if necessary
+# 2. Deskew and/or despeckle with imagemagick
+# 3. Use unpaper to split the pages and do its cleanup
+# 4. Add Fred's textcleaner?
+# 5. Stop here to manually check the files.
 
 s=''
 j=0
@@ -22,7 +23,6 @@ unpaperOptions="--no-border-align" # Supposed to be the default, but isn't
 
 # set up functions to report Usage and Usage with Description
 PROGNAME=`type convert | awk '{print $3}'`  # search for convert executable on path
-PROGDIR=`dirname $PROGNAME`                 # extract directory of program
 PROGNAME=`basename $PROGNAME`               # base name of program
 
 # Read flags
@@ -96,6 +96,7 @@ done
 
 # Remaining input should be the filename
 input="$1"
+input_extension="${1##*.}"
 
 # Make sure file exists
 if [[ ! -s "$1" ]]
@@ -104,6 +105,23 @@ then
   echo -e "\a"File "$1" does not appear to exist! Exiting...
   echo ''
   exit 0
+fi
+
+# Throw warning when nothing to be done
+if [[ $input_extension != 'pdf' && $layout == '' && $deskew == '' && $despeckle == '' ]]
+then
+    echo ''
+    echo  -e "\a    Oops! Nothing will happen to non-PDF files unless some processing is specified."
+    echo ''
+    exit 0
+fi
+
+# Throw warning when deskew and double are both applied
+if [[ $deskew != '' && $layout == 'double' ]]
+then
+    echo ''
+    echo  -e "\a    Generally speaking deskewing a two-page layout has minimal effect."
+    echo ''
 fi
 
 # Use file's directory for temporary files
@@ -115,19 +133,36 @@ unpaperdir="unpaper_$number"
 unpaper2dir="unpaper2_$number"
 unpaper3dir="unpaper3_$number"
 cleandir="clean_$number"
-workingdir=$convertdir
 
-mkdir "$dir/$convertdir"
-pdfimages $outputFormat "$1" "$dir/$convertdir/$output"
-
-input=`ls "$dir/$convertdir/$output"* | head -n 1`
-extension="${input##*.}"
+if [[ $input_extension == 'pdf' ]]
+then
+    mkdir "$dir/$convertdir"
+    pdfimages $outputFormat "$1" "$dir/$convertdir/$output"
+    # Get filetype that was output
+    input=`ls "$dir/$convertdir/$output"* | head -n 1`
+    extension="${input##*.}"
+    origin_dir="$dir/$convertdir"
+    workingdir="$convertdir"
+else
+    extension=$input_extension
+    origin_dir="$dir"
+    firstchar=`basename "$1"`
+    firstchar=${firstchar:0:1}
+    workingdir=''
+fi
 
 if [[ $deskew != '' || $despeckle != '' ]]
 then
+  extension='png'
   mkdir "$dir/$cleandir"
-  workingdir=$cleandir
-  convert $deskew $despeckle "$dir/$convertdir/$output-"* "$dir/$cleandir/$output-%03d.$extension" 1>/dev/null
+  workingdir="$cleandir"
+  if [[ $input_extension == 'pdf' ]]
+  then
+    search_string="$origin_dir/$output-"*
+  else
+    search_string="$origin_dir/$firstchar"*".$input_extension"
+  fi
+  convert $deskew $despeckle "$search_string" "$dir/$cleandir/$output"-%03d.$extension 1>/dev/null
 fi
 
 # Use unpaper on scans with 2 pages per image or if requested
@@ -139,24 +174,26 @@ then
   then
     mkdir "$dir/$unpaper2dir/"
   fi
-  
+
   # Need a workaround for unpaper not starting counting at 00, but 01
-  for i in `ls "$dir/$convertdir/"`
+  for i in "$dir"/"$workingdir"/*."$extension"
   do
-	k="00$j"
+    k="00$j"
 	l=${k: -3}
+	input_name=`basename "$i"`
+	input_name=${input_name%%.*}
 	if [[ $twice = true ]]
 	then
-	  unpaper "$unpaperOptions" -l $layout -op 1 "$dir/$workingdir/$i" "$dir/$unpaper2dir/$i"
-	  unpaper "$unpaperOptions" -l $layout -op 2 "$dir/$unpaper2dir/$i" "$dir/$unpaperdir/$output-$l%02d.$extension"
+	  unpaper "$unpaperOptions" -l $layout -op 1 "$i" "$dir/$unpaper2dir/$input_name".pgm
+	  unpaper "$unpaperOptions" -l $layout -op 2 "$dir/$unpaper2dir/$input_name".pgm "$dir/$unpaperdir/$output-$l-%02d".pgm
 	else
-	if [[ $op -eq 1 ]]
-	then
-	  # Keep the same file name when image has a single page on it
-	  unpaper -l $layout -op $op "$dir/$workingdir/$i" "$dir/$unpaperdir/$output-$l.$extension"
-	else
-	  unpaper -l $layout -op $op "$dir/$workingdir/$i" "$dir/$unpaperdir/$output$l%02d.pgm"
-	fi
+	  if [[ $op -eq 1 ]]
+	  then
+	    # Keep the same file name when image has a single page on it
+        unpaper -l $layout -op $op "$i" "$dir/$unpaperdir/$output-$l".pgm
+      else
+        unpaper -l $layout -op $op "$i" "$dir/$unpaperdir/$output"-"$l"-%02d.pgm
+	  fi
 	fi
     ((j++))
   done
