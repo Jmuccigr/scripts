38598b03d41adcde8a7a862f83fdaaca73b15549
John Muccigrosso <john@muccigrosso.org>
2023-02-07 18:52:59 +0100

    pypdf for pyPDF2; Monterey homebrew

--- remove_PDF_text.py
+++ remove_PDF_text.py
@@ -1,19 +1,18 @@
-#!/usr/local/bin/python3
-
+#!/opt/homebrew/bin/python3
 import sys
-from PyPDF2 import PdfFileWriter, PdfFileReader
-output = PdfFileWriter()
+from pypdf import PdfWriter, PdfReader
+output = PdfWriter()
 
 filename = str(sys.argv[1])
 outputname = str(sys.argv[2])
 
-ipdf = PdfFileReader(open(filename, 'rb'))
+ipdf = PdfReader(open(filename, 'rb'))
 
-for i in range(ipdf.getNumPages()):
-	page = ipdf.getPage(i)
-	output.addPage(page)
-	output.removeText(i)
+#for i in range(ipdf.getNumPages()):
+for i in range(len(ipdf.pages)):
+	page = ipdf.pages[i]
+	output.add_page(page)
+	output.remove_text
 
 with open(outputname, 'wb') as f:
    output.write(f)
-
