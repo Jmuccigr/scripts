b859ad36345bf4f0e2979f1fd9d4038685756013
John Muccigrosso <john@muccigrosso.org>
2023-03-09 10:42:25 +0100

    Working version after pypdf update

--- remove_PDF_text.py
+++ remove_PDF_text.py
@@ -10,9 +10,12 @@ ipdf = PdfReader(open(filename, 'rb'))
 
 #for i in range(ipdf.getNumPages()):
 for i in range(len(ipdf.pages)):
-	page = ipdf.pages[i]
-	output.add_page(page)
-	output.remove_text
+    page = ipdf.pages[i]
+    output.add_page(page)
+
+output.remove_text()
+
 
 with open(outputname, 'wb') as f:
    output.write(f)
+
