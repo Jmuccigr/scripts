d6ac977c649bdbfad23694cee5247f3394ef1bb1
John Muccigrosso <john@muccigrosso.org>
2025-07-29 17:34:43 +0200

    More robust; YYYY-MM-DD input

--- dates/weekdayByZeller.sh
+++ dates/weekdayByZeller.sh
@@ -12,29 +12,63 @@
 # need to convert them:
 #
 # q = day of the month (1-31)
-# m = month where March = 3 & January & February are 13 & 14 (3-14) of 
+# m = month where March = 3 & January & February are 13 & 14 (3-14) of
 #     the preceding year
 # y = year (for reliability, from 8 on)
 
 ################## Functions ####################
-  
+
+# Pass the date as 'YYYY MM DD'. You can use a non-numerical space between the
+# numbers, like '.' or '/' or '-'. (Actually it can be anything as long as it
+# doesn't contain a number.)
+function parsedate () {
+    if [ $# == 3 ]
+    then
+        yr=$1
+        mo=$2
+        d=$3
+    elif [ $# == 1 ]
+    then
+        num='([0-9]+)'
+        spacer='[^0-9]+'
+        if [[ $1 =~ $num$spacer$num$spacer$num ]] ; then
+          yr=${BASH_REMATCH[1]}
+          mo=${BASH_REMATCH[2]}
+          d=${BASH_REMATCH[3]}
+        fi
+    else # There's some other number of arguments
+        badDate=true
+    fi
+    # Quick test of the values
+    # Could do more here, like checking month and day numbers.
+    if [[ $(( yr * mo * d )) == 0 ]]
+    then
+        badDate=true
+    fi
+    if [ $badDate ]
+    then
+        echo "That wasn't a date."
+        exit 1
+    fi
+}
+
+# Use Zeller's Congruence to calculate the day of the week from the date.
 function zeller () {
   local weekdays=("Saturday" "Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday")
-  local q m mo y J K
-  # Use Zeller's Congruence to calculate the day of the week
-  q=$1
-  mo=$2
-  yr=$3
+  local q m y J K
+
+  y=$1
+  m=$2
+  q=$3
+
   # Adjust for Jan and Feb
-  if [[ $mo -lt 3 ]]; then
-    m=$(( mo + 12 ))
-    y=$(( yr - 1 ))
-  else
-    m=$mo
-    y=$yr
+  if [[ $m -lt 3 ]]; then
+    m=$(( m + 12 ))
+    y=$(( y - 1 ))
   fi
   J=$(( y / 100 ))
   K=$(( y % 100 ))
+
   # Fix the date for the skipped Gregorian days by adding 10 to days in the gap
   [[ $y -eq 1582 && $m -eq 10 && $q -gt 4 && $q -lt 15 ]] && q=$(( q + 10 ))
   if [ $y -lt 1582 ] || ( [ $y -eq 1582 ] && [ $mo -eq 10 ] && [ $q -lt 15 ] ); then
@@ -45,13 +79,14 @@ function zeller () {
     dayoftheweek=$(( (q + ( ((13 * ( m + 1 ))) / 5) + K + ( K / 4 ) + ( J / 4 ) - 2*J ) % 7 ))
   fi
   [[ $dayoftheweek -lt 0 ]] && dayoftheweek=$(( 7 + dayoftheweek ))
-  echo -n "$yr "
   weekday=${weekdays[$dayoftheweek]}
 }
 
 ################ End functions ##################
 
-zeller $1 $2 $3
+parsedate $@
+zeller $yr $mo $d
+
 [[ $? == 0 ]] && echo $weekday || echo "Something went wrong"
 
 
