18c83b6190dfe3b189b554c8e1c8b7ea440fe7f6
John Muccigrosso <john@muccigrosso.org>
2026-03-07 15:07:00 +0100

    New function; remove UC for non-constant

--- local_utils.py
+++ local_utils.py
@@ -1,13 +1,18 @@
-from datetime import datetime
+from datetime import date, timedelta, timezone, datetime
 import sys
 import subprocess
+import time
 
 def send_mail(recipient, subject, message):
     sendmail_command = f"echo '{message}' | mail -s '{subject}' {recipient}"
     subprocess.run(sendmail_command, shell=True)
 
 def log_this(msg, exit):
-    TIMESTAMP = (f'{datetime.now():%Y-%m-%d %H:%M:%S%z}')
-    print(TIMESTAMP + " " + msg, file=sys.stderr)
+    timestamp = (f'{datetime.now():%Y-%m-%d %H:%M:%S%z}')
+    print(timestamp, msg, file=sys.stderr)
     if exit:
         sys.exit()
+
+def get_yesterday(days_before):
+    yesterday = date.today() - timedelta(days=days_before)
+    return yesterday
